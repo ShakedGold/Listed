998ad474045873766cfdaab383b2968799656f94
ShakedGold <shakedgold2005@gmail.com>
2023-06-26 23:35:28 +0300

    drag & drop

--- src/components/Dropover.vue
+++ src/components/Dropover.vue
@@ -1,44 +1,38 @@
-<script>
+<script setup>
+import { ref } from 'vue'
+
+const dragging = ref(false);
+
 function drop(e) {
     e.preventDefault();
-    this.isDragging = false;
-    this.onChange();
+    dragging.value = false;
 }
 
 function dragover(e) {
     e.preventDefault();
-    this.isDragging = true;
+    dragging.value = true;
 }
 
 function dragleave(e) {
     e.preventDefault();
-    this.isDragging = false;
+    dragging.value = false;
+}
+
+function isDragging() {
+    return dragging.value;
 }
 </script>
 
 <template>
     <div class="main">
-    <div
-      class="dropzone-container"
-      @dragover="dragover"
-      @dragleave="dragleave"
-      @drop="drop"
-    >
-      <input
-        type="file"
-        multiple
-        name="file"
-        id="fileInput"
-        class="hidden-input"
-        @change="onChange"
-        ref="file"
-        accept=".pdf,.jpg,.jpeg,.png"
-      />
+        <div class="dropzone-container" @dragover="dragover" @dragleave="dragleave" @drop="drop">
+            <input type="file" multiple name="file" id="fileInput" class="hidden-input" @change="onChange" ref="file"
+                accept=".pdf,.jpg,.jpeg,.png" />
 
-      <label for="fileInput" class="file-label">
-        <div v-if="isDragging">Release to drop files here.</div>
-        <div v-else>Drop files here or <u>click here</u> to upload.</div>
-      </label>
+            <label for="fileInput" class="file-label">
+                <div v-if="dragging">Release to drop files here.</div>
+                <div v-else>Drop files here or <u>click here</u> to upload.</div>
+            </label>
+        </div>
     </div>
-  </div>
 </template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import MenuBar from '../components/MenuBar/MenuBar.vue'
+import MenuBar from '@/components/MenuBar/MenuBar.vue'
 </script>
 
 <template>
