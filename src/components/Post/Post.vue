e1ad60bfc92bc0e114b31307dfc25216a7962d3c
nitai shaar <nitais@netanya.ac.il>
2023-06-27 13:45:19 +0300

    Post Change

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="post">
         <div id="post-actions">
-            <p>Upvote</p>
+            <p class="Votes">Upvote</p>
         </div>
         <div id="post-body">
             <p id="post-title">This is water</p>
@@ -14,21 +14,37 @@
 
 <style>
 #post {
-    display: grid;
-    justify-content: center;
-    align-items: center;
+    display: flex;
+    padding: 0%;
+    width: 650px;
+    border: solid 1px black;
 }
 #post-actions {
     background-color: rgb(0, 255, 0);
 }
 #post-body {
-    width: 30%;
+    width: 100%;
 }
 #post-title {
+    width: 100%;
+    height: 10%;
+    margin-top: 0%;
+    margin-bottom: 0%;
     background-color: red;
 }
 #post-img {
+    margin-top: 0%;
     object-fit: cover;
-    width: 500px;
+    width: 100%;
+    height: 90%;
+}
+.Votes{
+    background-color: rgb(0, 255, 0);
+    height: 15em;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 100px;
+    font-size: 20px;
+    font-weight: bold;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,4 +4,5 @@ import Post from "@/components/Post/Post.vue"
 
 <template>
     <Post />
+    
 </template>
