1172a974654c6ee734f9e17654e4025f59eab4e7
ShakedGold <shakedgoldy@gmail.com>
2023-07-30 20:27:25 +0300

    HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Post from "@/components/Post/Post.vue"
-import { ref } from "vue"
-import { query, orderBy, getDocs } from "firebase/firestore";
+import Post from "@/components/Post/Post.vue";
+import { getDocs, orderBy, query } from "firebase/firestore";
+import { ref } from "vue";
 
 import { postsRef } from "@/scripts/firebase";
 
@@ -11,44 +11,29 @@ let sortMode = ref("top");
 //array of all the posts
 let posts = ref([]);
 
-const q = query(postsRef, orderBy('votes', "desc"));
+const q = query(postsRef, orderBy("votes", "desc"));
 const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc) => {
-  posts.value.push(doc.data());
+	posts.value.push(doc.data());
 });
 </script>
 
 <template>
-  <MenuBar />
-  <div id="sorting-container">
-    <p>Sort By: </p>
-    <select name="sort-selection" id="sort-selection" v-model="sortMode">
-      <option value="new">New</option>
-      <option value="top">Top</option>
-    </select>
-  </div>
-  <div id="post-container" v-if="posts.length !== 0">
-    <Post v-for="post in posts" :postObj="post" />
-  </div>
-  <div v-else>
-    <h1>So Empty...</h1>
-  </div>
+	<MenuBar />
+	<div>
+		<p>Sort By:</p>
+		<select name="sort-selection" id="sort-selection" v-model="sortMode">
+			<option value="new">New</option>
+			<option value="top">Top</option>
+		</select>
+	</div>
+	<div
+		class="flex flex-col gap-24 justify-center align-middle"
+		v-if="posts.length !== 0"
+	>
+		<Post v-for="post in posts" :postObj="post" />
+	</div>
+	<div v-else>
+		<h1>So Empty...</h1>
+	</div>
 </template>
-
-<style>
-body {
-  background-color: #e7ecef;
-  margin: 0;
-  padding: 0;
-  font-size: 1.5em;
-  color: black;
-}
-
-#post-container {
-  display: flex;
-  flex-direction: column;
-  gap: 100px;
-  justify-content: center;
-  align-items: center;
-}
-</style>
