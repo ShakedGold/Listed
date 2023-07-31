20cd7585e7ad000ab326fb76c26a2c711de2ea6a
Michal Dvir <Michal.Dvir@mercure.co.il>
2023-07-31 16:23:43 +0300

    follow function

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,15 +1,24 @@
+//import { getCurrentUser} from "../scripts/auth.js";
+//let currentUser =  getCurrentUser();
+
 export class User {
-  constructor(email, username, following, postInteractions) {
+  constructor(email, username, following, followers, postInteractions) {
     this.email = email;
     this.username = username;
     this.following = following;
+    this.followers = followers;
     this.postInteractions = postInteractions;
   }
   toString() {
-    return this.email + ', ' + this.username + ', ' + this.following + ', ' + this.postInteractions;
+    return this.email + ', ' + this.username + ', ' + this.following + ', ' + this.followers + ', ' + this.postInteractions;
+  }
+  follow(user) {
+    alert(this.username + user.username);
+    this.followers+=user;
+    user.following+=this;
   }
 }
-
+  
 // Firestore data converter
 export const userConverter = {
   toFirestore: (user) => {
@@ -17,11 +26,12 @@ export const userConverter = {
       email: user.email,
       username: user.username,
       following: user.following,
+      followers: user.followers,
       postInteractions: user.postInteractions,
     };
   },
   fromFirestore: (snapshot, options) => {
     const data = snapshot.data(options);
-    return new User(data.email, data.username, data.following, data.postInteractions);
+    return new User(data.email, data.username, data.following, data.followers, data.postInteractions);
   }
 };
