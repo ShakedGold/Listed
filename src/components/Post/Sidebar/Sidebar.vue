c5027d77788cf7d61a6d55c044ec4264f91bd871
nitai shaar <nitais@netanya.ac.il>
2023-08-04 17:40:39 +0300

    add report button

--- src/components/Post/Sidebar/Sidebar.vue
+++ src/components/Post/Sidebar/Sidebar.vue
@@ -5,6 +5,8 @@ import { Interaction as InteractionEnum } from '../../../classes/Interaction';
 import { doc, updateDoc } from 'firebase/firestore';
 
 import { postsRef, usersRef } from '../../../scripts/firebase';
+import { ref } from 'vue';
+import ConfirmModal from '../../Modal/ConfirmModal.vue';
 
 let props = defineProps({
   user: {
@@ -16,6 +18,12 @@ let props = defineProps({
     required: true,
   },
 });
+let open = ref(false);
+let reports = ref([
+  'Spam',
+  'Inappropriate',
+  'Other',
+]);
 
 function interact(interaction) {
   if (props.user.postInteractions[props.post.ID] === interaction) {
@@ -30,12 +38,16 @@ function interact(interaction) {
     props.user.postInteractions[props.post.ID] = interaction;
     props.post.votes += interaction;
   }
-
   if (props.user.postInteractions[props.post.ID] === InteractionEnum.None) {
     delete props.user.postInteractions[props.post.ID];
   }
 
   update();
+}
+
+function Report() {
+  open.value = true;
+
 }
 async function updatePost() {
   const postDocRef = doc(postsRef, props.post.ID);
@@ -54,15 +66,30 @@ async function update() {
 </script>
 
 <template>
-  <div class="text-center border-black border-r-2">
+  <ConfirmModal :open="open" :on-cancel="() => open = false">
+    <template #header>
+      <h1 class="text-2xl">Report Post</h1>
+    </template>
+    <template #body>
+      <div class="flex flex-col">
+        <span class="flex gap-1" v-for="report in reports">
+          <input type="checkbox" :id="report" />
+          <label :for="report">{{ report }}</label>
+        </span>
+      </div>
+    </template>
+  </ConfirmModal>
+  <div class="text-center border-black border-r-2 relative">
     <Interaction class="object-cover w-[60px]" text="like"
       icon-url="https://media.geeksforgeeks.org/wp-content/uploads/20220529211152/up-300x300.png"
       :click-fn="() => interact(InteractionEnum.Liked)"
       :class="user.postInteractions[post.ID] === InteractionEnum.Liked ? 'bg-blue-400 rounded-3xl' : ''" />
-    <span class="text-xl">{{ post.votes }}</span>
+    <span class="">{{ post.votes }}</span>
     <Interaction class="object-cover w-[60px]" text="like"
       icon-url="https://media.geeksforgeeks.org/wp-content/uploads/20220529211152/down-300x300.png"
       :click-fn="() => interact(InteractionEnum.Disliked)"
       :class="user.postInteractions[post.ID] === InteractionEnum.Disliked ? 'bg-blue-400 rounded-3xl' : ''" />
+    <Interaction class="object-cover w-[60px] absolute bottom-0"
+      icon-url="https://cdn-icons-png.flaticon.com/128/4201/4201965.png" :click-fn="Report"></Interaction>
   </div>
 </template>
