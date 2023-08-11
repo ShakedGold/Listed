a18e61dea9681ee21d9ea68be065e289440b7646
ShakedGold <shakedgoldy@gmail.com>
2023-08-11 14:21:24 +0300

    ListSearch done and JSDoc Implementation on services

--- src/components/Post/ListSearch.vue
+++ src/components/Post/ListSearch.vue
@@ -5,11 +5,14 @@ import { listsRef } from "@/services/firebase";
 import { Search } from "../../services/Algorithm";
 
 let props = defineProps(["modelValue"]);
-defineEmits(["update:modelValue", "addNewList"]);
+let emit = defineEmits(["update:modelValue", "addNewList"]);
 
 let expandList = ref(false);
+let selected = ref(0);
+let isHovering = ref(false);
 
 const querySnapshot = await getDocs(listsRef);
+
 let lists = ref(
 	querySnapshot.docs
 		.filter(
@@ -18,10 +21,12 @@ let lists = ref(
 		)
 		.map((doc) => doc.data().name)
 );
+let options = computed(() =>
+	Search(props.modelValue, lists.value, props.modelValue === "").concat(
+		"Add new list"
+	)
+);
 
-let options = computed(() => Search(props.modelValue, lists.value));
-
-// Add a method to handle the unfocus event
 const handleBlur = () => {
 	// Use a timeout to check if a button was clicked before hiding the list
 	setTimeout(() => {
@@ -31,16 +36,63 @@ const handleBlur = () => {
 		});
 	}, 100);
 };
+
+const handleKeyDown = (event) => {
+	const index = selected.value || 0;
+	isHovering.value = false;
+	switch (event.key) {
+		case "ArrowDown":
+			event.preventDefault();
+			// if the current selected item is the last item in the list, select the first item
+			if (index + 1 >= options.value.length) {
+				//emit("update:modelValue", options.value[0]);
+				selected.value = 0;
+			} else {
+				// select the next item in the list
+				//emit("update:modelValue", options.value[index + 1]);
+				selected.value = index + 1;
+			}
+			break;
+		case "ArrowUp":
+			event.preventDefault();
+			// if the current selected item is the last item in the list, select the first item
+			if (index - 1 < 0) {
+				// select the next item in the list
+				//emit("update:modelValue", options.value[index + 1]);
+				selected.value = options.value.length - 1;
+			} else {
+				// select the next item in the list
+				//emit("update:modelValue", options.value[index + 1]);
+				selected.value = index - 1;
+			}
+			break;
+		case "Enter":
+			event.preventDefault();
+			if (options.value[index] === "Add new list") {
+				emit("addNewList");
+			} else {
+				emit("update:modelValue", options.value[index]);
+			}
+			expandList.value = false;
+			selected.value = 0;
+			break;
+	}
+};
 </script>
 
 <template>
-	<div class="relative">
+	<div class="relative" @keydown="handleKeyDown">
 		<input
 			type="text"
 			placeholder="Search a list"
 			:value="modelValue"
 			class="border-2 border-gray-300 rounded-md p-2 outline-none focus:border-accent"
-			@input="$emit('update:modelValue', $event.target.value)"
+			@input="
+				($event) => {
+					$emit('update:modelValue', $event.target.value);
+					expandList = true;
+				}
+			"
 			@focus="expandList = true"
 			@blur="handleBlur"
 		/>
@@ -49,27 +101,39 @@ const handleBlur = () => {
 			class="bg-white absolute top-11 left-0 border-2 p-2 rounded-md grid place-items-start"
 		>
 			<button
-				class="hover:bg-neutral-200 rounded-md p-2 w-full text-left"
+				class="rounded-md p-2 w-full text-left"
+				@mouseenter="
+					() => {
+						isHovering = true;
+						selected = options.indexOf(option);
+					}
+				"
+				@mouseleave="isHovering = false"
+				:class="{
+					'bg-neutral-200': option === options[selected] && !isHovering,
+					'hover:bg-neutral-200': isHovering,
+				}"
+				@mousemove="
+					() => {
+						if (!isHovering) {
+							selected = options.indexOf(option);
+						}
+					}
+				"
 				@click="
 					($event) => {
-						$emit('update:modelValue', $event.target.textContent);
+						$emit(
+							'update:modelValue',
+							$event.target.textContent === 'Add new list'
+								? $emit('addNewList')
+								: option
+						);
 						expandList = false;
 					}
 				"
 				v-for="option in options"
 			>
 				{{ option }}
-			</button>
-			<button
-				class="hover:bg-neutral-200 rounded-md p-2 w-full text-left"
-				@click="
-					() => {
-						$emit('addNewList');
-						expandList = false;
-					}
-				"
-			>
-				Add new list
 			</button>
 		</div>
 	</div>
--- src/services/Algorithm.js
+++ src/services/Algorithm.js
@@ -1,15 +1,41 @@
-export function Search(term, options) {
+/**
+ * This service contains a collection of Algorithms that are usefull.
+ * @module
+ */
+
+/**
+ * Searches through the options for the term and returns a filtered and sorted list of options using Rank.
+ * @author ShakedGold <shakedgold@listed.com>
+ * @param {string} term the partial term to search for (e.g. "abc")
+ * @param {string[]} options the options to search through (e.g. ["abcdef", "def", "ghi"])
+ * @param {boolean} dontUseFilter if true, the filter will not be used, and all options will be returned
+ * @return {string[]} the options sorted by rank (e.g. ["abcdef", "ghi", "def"])
+ * @example Search("de", ["def", "abcdef", "ghi"]) -> ["def", "abcdef"];
+ */
+export function Search(term, options, dontUseFilter = false) {
+	const bias = 0.5; // 0.5 is the default value
+	// functions to be used in the filter and sort
+	const rankBiasFunction = (term, option) => Rank(term, option) / term.length;
+	const sortCompareFunction = (optionA, optionB) =>
+		Rank(term, optionB) - Rank(term, optionA);
 	return options
-		.filter((option) => term === "" || Rank(term, option) / term.length > 0.5)
-		.sort((a, b) => Rank(term, b) - Rank(term, a));
+		.filter((option) => dontUseFilter || rankBiasFunction(term, option) >= bias)
+		.sort((optionA, optionB) => sortCompareFunction(optionA, optionB));
 }
 
+/**
+ * Ranks the term in the option by counting the number of letters in the term that are in the option not depending on the case.
+ * @author ShakedGold <shakedgold@listed.com>
+ * @param {string} term the partial term to search for (e.g. "abc")
+ * @param {string} option the option to search through (e.g. "AbcDef")
+ * @return {number} the rank of the term in the option (e.g. 3 for "abc" in "abcdef")
+ */
 export function Rank(term, option) {
 	let rank = 0;
-	for (let i = 0; i < term.length; i++) {
-		if (option.toLowerCase().includes(term[i].toLowerCase())) {
+	[...term].forEach((letter) => {
+		if (option.toLowerCase().includes(letter.toLowerCase())) {
 			rank++;
 		}
-	}
+	});
 	return rank;
 }
