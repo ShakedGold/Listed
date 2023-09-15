4cf306fb03d991eae333f2176465b14df6cd3df4
ShakedGold <shakedgoldy@gmail.com>
2023-09-15 17:56:13 +0300

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +14,6 @@ export default defineConfig({
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
-	}
+	},
+	base: '/Listed/',
 });
