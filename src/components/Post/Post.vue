e004f5f1ebbc6e3c5067ebd6dce60813bae6f7a2
ShakedGold <shakedgoldy@gmail.com>
2023-09-23 22:29:52 +0300

    fix gotopost on user click

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -10,10 +10,10 @@ import router from '../../router';
 import { getCurrentUserOrNew } from '../../services/auth';
 
 const props = defineProps({
-	post: {
-		type: Post,
-		required: true,
-	},
+  post: {
+    type: Post,
+    required: true,
+  },
 });
 
 const hex = ref(props.post.color.hex);
@@ -21,27 +21,27 @@ const isHovering = ref(false);
 
 const user = ref(await getCurrentUserOrNew());
 const stopHovering = () => {
-	isHovering.value = false;
+  isHovering.value = false;
 };
 
 const chunks = props.post.text.split(' ').reduce((acc, cur) => {
-	if (acc.length === 0)
-		acc.push(cur);
-	 else if (acc[acc.length - 1].length + cur.length + 1 > 100)
-		acc.push(cur);
-	 else
-		acc[acc.length - 1] += ` ${cur}`;
-	
-	return acc;
+  if (acc.length === 0)
+    acc.push(cur);
+  else if (acc[acc.length - 1].length + cur.length + 1 > 100)
+    acc.push(cur);
+  else
+    acc[acc.length - 1] += ` ${cur}`;
+
+  return acc;
 }, []);
 
 function goToPost() {
-	router.push({
-		name: 'Post',
-		params: {
-			id: props.post.ID,
-		},
-	});
+  router.push({
+    name: 'Post',
+    params: {
+      id: props.post.ID,
+    },
+  });
 }
 </script>
 
@@ -51,29 +51,12 @@ function goToPost() {
     :style="{
       'background-color': isHovering ? hex : '#E2E2E2',
       'color': isHovering && post.color.isDark ? '#fff' : '#000',
-    }"
-    @mouseenter="isHovering = true"
-    @mousemove="isHovering = true"
-    @mouseleave="stopHovering"
-  >
-    <Title
-      :post="post"
-      @click="goToPost"
-    />
+    }" @mouseenter="isHovering = true" @mousemove="isHovering = true" @mouseleave="stopHovering">
+    <Title :post="post" />
     <div class="relative z-0">
-      <Body
-        :post="post"
-        :is-hovering="isHovering"
-        :hex="hex"
-        :chunks="chunks"
-        @click="goToPost"
-      />
-      <Interactions
-        :is-hovering="isHovering"
-        :post="post"
-        :user="user"
-        :chunks="chunks"
-      />
+
+      <Body :post="post" :is-hovering="isHovering" :hex="hex" :chunks="chunks" @click="goToPost" />
+      <Interactions :is-hovering="isHovering" :post="post" :user="user" :chunks="chunks" />
     </div>
   </div>
 </template>
